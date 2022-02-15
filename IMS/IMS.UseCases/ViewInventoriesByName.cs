0da347207f8ab81ed491bfae0c24133969293bfd
Alex Dopico <alex.dopico@gmail.com>
2022-02-15 11:51:46 +0100

    .

--- IMS/IMS.UseCases/ViewInventoriesByName.cs
+++ IMS/IMS.UseCases/ViewInventoriesByName.cs
@@ -1,10 +1,20 @@
-﻿namespace IMS.UseCases
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases
 {
     public class ViewInventoriesByName
     {
-        public IEnumerable<Inventory> ExecuteAsync(string name)
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoriesByName(IInventoryRepository inventoryRepository)
         {
 
+        }
+
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name)
+        {
+            return await this.inventoryRepository.GetInventoriesByName(name);
         }
     }
 }
