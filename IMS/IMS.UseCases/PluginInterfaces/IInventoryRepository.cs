ac73bb80c544874ffd20a333b75d5e5709dd9794
Alex Dopico <alex.dopico@gmail.com>
2022-02-15 13:30:38 +0100

    .

--- IMS/IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
+++ IMS/IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using IMS.CoreBusiness;
+
 namespace IMS.UseCases.PluginInterfaces
 {
     public interface IInventoryRepository
--- IMS/IMS.WebApp/Program.cs
+++ IMS/IMS.WebApp/Program.cs
@@ -1,13 +1,20 @@
+using IMS.Plugins.EFCore;
 using IMS.WebApp;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>
+builder.Services.AddDbContext<IMSContext>(options =>
+{
+    options.UseInMemoryDatabase("IMS");
+});
 
 builder.Services.AddOidcAuthentication(options =>
 {
@@ -17,3 +24,10 @@ builder.Services.AddOidcAuthentication(options =>
 });
 
 await builder.Build().RunAsync();
+
+var app = builder.Build();
+var scope = app.Services.CreateScope();
+var imsContext = scope.ServiceProvider.GetRequiredService<IMSContext>();
+imsContext.Database.EnsureDeleted();
+imsContext.Database.EnsureCreated();
+
